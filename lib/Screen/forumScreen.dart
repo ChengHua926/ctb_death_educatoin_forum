d62eb7e8dc678a93d04257e61596429d023d30f5
ChengHua926 <huacheng615@gmail.com>
2023-02-25 13:08:51 -0500

    code from github
    not gonna need this anymore, just use github's ui

--- lib/Screen/forumScreen.dart
+++ lib/Screen/forumScreen.dart
@@ -11,34 +11,74 @@ class _homeScreenState extends State<homeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.tealAccent,
-      appBar: AppBar(
-        title: Text('APP BAR'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Container(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+          child: ListView(
+        children: <Widget>[
+          Container(
+            height: 160,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+            child: Padding(
+              padding: EdgeInsets.all(12.0),
               child: Column(
-                children: [
-                  Expanded(child: Containerr),
-                  Expanded(child: child),
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Sra, Forum",
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(height: 8.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Find Topics you like to read",
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.6),
+                          fontSize: 14.0,
+                        ),
+                      ),
+                      Icon(
+                        Icons.search,
+                        size: 20,
+                        color: Colors.white,
+                      )
+                    ],
+                  )
                 ],
               ),
             ),
           ),
-          Expanded(
-            flex: 3,
-            child: Container(
+          Container(
               decoration: BoxDecoration(
-                color: Colors.teal,
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              ),
-            ),
-          ),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(35.0),
+                      topRight: Radius.circular(35.0))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        EdgeInsets.only(left: 20.0, top: 20.0, bottom: 10.0),
+                    child: Text(
+                      "Trending Posts",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ))
         ],
-      ),
+      )),
     );
   }
 }
